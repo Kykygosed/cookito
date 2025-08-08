6229abde88d9527f26a6de537888e427232de62d
Kykygosed <143290781+Kykygosed@users.noreply.github.com>
2025-08-09 00:40:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // --- Configuration Firebase ---
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // Mets ta config Firebase ici
-  apiKey: "TA_CLE_API",
-  authDomain: "TON_DOMAINE.firebaseapp.com",
-  databaseURL: "https://TON_DOMAINE.firebaseio.com",
-  projectId: "TON_PROJET",
-  storageBucket: "TON_PROJET.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
+  apiKey: "AIzaSyDyWu4TI4PIRXfeb7yqt0WIGClgu10IjkM",
+  authDomain: "kylita-f2923.firebaseapp.com",
+  databaseURL: "https://kylita-f2923-default-rtdb.firebaseio.com",
+  projectId: "kylita-f2923",
+  storageBucket: "kylita-f2923.firebasestorage.app",
+  messagingSenderId: "431823530994",
+  appId: "1:431823530994:web:88a07e633751686e5ad96b",
+  measurementId: "G-F4LLNWQJ16"
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
